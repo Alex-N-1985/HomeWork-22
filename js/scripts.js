cf7f593e7ee7d5895447e6fff465eb5a11ed8b36
Alex <42239536+RobertAnderson1985@users.noreply.github.com>
2024-01-10 17:55:35 +0300

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,64 @@
+$houses = [
+    {
+        houseImg: 'House21.png',
+        header: 'Атаман',
+        square: '108 м',
+        size: '7x9',
+        constrPeriod: '16 дней',
+        numRooms: '5',
+        houseType: 'Каркасный',
+        price: '1 780 000'
+    },
+    {
+        houseImg: 'House21.png',
+        header: 'Атаман',
+        square: '108 м',
+        size: '7x9',
+        constrPeriod: '16 дней',
+        numRooms: '5',
+        houseType: 'Каркасный',
+        price: '1 780 000'
+    },
+    {
+        houseImg: 'House21.png',
+        header: 'Атаман',
+        square: '108 м',
+        size: '7x9',
+        constrPeriod: '16 дней',
+        numRooms: '5',
+        houseType: 'Каркасный',
+        price: '1 780 000'
+    },
+    {
+        houseImg: 'House21.png',
+        header: 'Атаман',
+        square: '108 м',
+        size: '7x9',
+        constrPeriod: '16 дней',
+        numRooms: '5',
+        houseType: 'Каркасный',
+        price: '1 780 000'
+    },
+    {
+        houseImg: 'House21.png',
+        header: 'Атаман',
+        square: '108 м',
+        size: '7x9',
+        constrPeriod: '16 дней',
+        numRooms: '5',
+        houseType: 'Каркасный',
+        price: '1 780 000'
+    },
+    {
+        houseImg: 'House21.png',
+        header: 'Атаман',
+        square: '108 м',
+        size: '7x9',
+        constrPeriod: '16 дней',
+        numRooms: '5',
+        houseType: 'Каркасный',
+        price: '1 780 000'
+    }
+];
+
+let popularproj = document.querySelector(".popularproj");
