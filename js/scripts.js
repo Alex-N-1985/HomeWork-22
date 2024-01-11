ab8135da07b628a201ffae30b25af2be7628f38b
Alex <42239536+RobertAnderson1985@users.noreply.github.com>
2024-01-12 01:53:35 +0300

    first commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-$houses = [
+let houses = [
     {
         houseImg: 'House21.png',
         header: 'Атаман',
@@ -62,3 +62,26 @@ $houses = [
 ];
 
 let popularproj = document.querySelector(".popularproj");
+let temp = null;
+
+for (let i = 0; i < houses.length; i++){
+    temp = createCard(houses[i]);
+    popularproj.appendChild(temp);
+}
+
+function createCard(cardData){
+    let card = document.createElement("div");
+    card.className = "card";
+
+    let cardHeader = document.createElement("h3");
+    cardHeader.className = "card__header font__preset";
+    cardHeader.innerText = cardData.header;
+
+    let cardImg = document.createElement("img");
+    cardImg.className = "card__image";
+    cardImg.src = './img/' + cardData.houseImg;
+
+    card.appendChild(cardImg);
+    card.appendChild(cardHeader);
+    return card;
+}
